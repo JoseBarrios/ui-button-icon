91b43b59e5617fcedb8039eceae72c9f0b4606ad
Jose Barrios <jose@cognilab.com>
2017-09-30 20:20:23 -0700

    Share buttons, front end implementation (design pending actions). A custom element with five different customizable actions, and icons ('share', 'email', 'link', 'facebook' and 'linkedin'). Pending custom icons, values and actions.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ui-share-button
-A button to share content 
+# ui-element-template
+A template element to get started
+
+#Test:
+
+1. npm test
+2. Open browser
+3. Go to IP
+4. Naviagte to tests > test.html
---
+++ element/ui-element-template-view-controller.js
@@ -0,0 +1,145 @@
+'use strict'
+
+const uiElementTemplateDoc = document._currentScript || document.currentScript;
+const uiElementTemplate = uiElementTemplateDoc.ownerDocument.querySelector('#ui-element-template-view');
+
+class ElementTemplate extends HTMLElement {
+
+  static get observedAttributes(){
+    return ['value', 'type', 'icon', 'size'];
+  }
+
+  constructor(){
+    super();
+    const view = document.importNode(uiElementTemplate.content, true);
+    this.shadowRoot = this.attachShadow({mode: 'open'});
+    this.shadowRoot.appendChild(view);
+		this.connected = false;
+
+		this.validTypes = ['share', 'link', 'email', 'facebook', 'linkedin'];
+		this.class = 'fa-share';
+		this.size=1;
+  }
+
+	///STANDARD
+	connectedCallback() {
+		//Wire views here
+    this.$container = this.shadowRoot.querySelector('.container');
+    this.$icon = this.shadowRoot.querySelector('#icon');
+
+		this.$container.addEventListener('click', this.clicked)
+    this._updateRendering();
+	}
+
+	clicked(e){
+		console.log('CONTAINER CLICKED')
+	}
+
+	adoptedCallback(){
+    //console.log('adoptedCallback');
+	}
+
+	disconnectedCallback() {
+		this.connected = false;
+		console.log('disconnected');
+	}
+
+	attributeChangedCallback(attrName, oldVal, newVal) {
+		switch(attrName){
+			case 'value':
+				this.value = newVal;
+				console.log('VALUE: ', newVal)
+				break;
+			case 'type':
+				//If type is invalid, default to 'share'
+				if(this.validTypes.includes(newVal)){ this.type = newVal; }
+				else { this.type = 'share'; }
+				console.log('TYPE: ', newVal)
+				break;
+			case 'icon':
+				this.icon = newVal;
+				console.log('ICON: ', newVal)
+				break;
+			case 'size':
+				this.size = newVal;
+				console.log('SIZE: ', newVal)
+				break;
+			default:
+				console.warn(`Attribute ${attrName} is not handled, you should probably do that`);
+		}
+  }
+
+  get shadowRoot(){return this._shadowRoot;}
+  set shadowRoot(value){ this._shadowRoot = value}
+
+	get value(){ return this._value; }
+	set value(value){
+		this._value = value;
+		this._updateRendering();
+		this._updateEvent()
+	}
+
+	get size(){ return this._size}
+	set size(value){
+		//Size can only be between 1-5
+		let number = Number(value);
+		number = number < 1? 1: number;
+		number = number > 5? 5: number;
+		this._size = number;
+	}
+
+	get type(){ return this._type; }
+	set type(attr){
+
+		this.validTypes = ['linkedin'];
+		switch(attr){
+			case 'share':
+				this.class = `fa fa-share`
+				break;
+			case 'email':
+				this.class = `fa fa-envelope`
+				break;
+			case 'link':
+				this.class = `fa fa-link`
+				break;
+			case 'facebook':
+				this.class = `fa fa-facebook`
+				break;
+			case 'linkedin':
+				this.class = `fa fa-linkedin`
+				break;
+			default:
+
+		}
+
+		this._type = attr;
+		this._updateRendering();
+		this._updateEvent()
+	}
+
+
+
+	_updateEvent(){
+		this.dispatchEvent(new CustomEvent(this.defaultEventName, {detail: 'TODO'}));
+	}
+
+  _updateRendering() {
+
+		if(this.$icon && this.$container){
+
+			this.$icon.className = `fa ${this.class}`;
+			this.$icon.style.fontSize = `${this.size}em`;
+			this.$icon.style.paddingTop = `0.5em`;
+			this.$icon.style.paddingLeft = `0.5em`;
+
+			this.$container.style.width = `${this.size * 2}em`
+			this.$container.style.height = `${this.size * 2}em`
+			this.$container.style.borderRadius = `${this.size}em`
+		}
+
+
+
+  }
+}
+
+window.customElements.define('ui-element-template', ElementTemplate);
